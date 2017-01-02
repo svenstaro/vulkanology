9c8a8db58f39e3f0efa7fd8ad9c655e52869373e
Alexander Dmitriev <alexander.dmitriev2580@gmail.com>
2017-01-02 16:26:18 +0100

    Export uninitialized buffer initialization to a macro

--- src/lib.rs
+++ src/lib.rs
@@ -63,7 +63,10 @@ macro_rules! instance {
 /// }
 /// {
 ///     // With some features:
-///     let physical_device = physical_device!(instance, robust_buffer_access, full_draw_index_uint32);
+///     let physical_device = physical_device!(
+///         instance,
+///         robust_buffer_access,
+///         full_draw_index_uint32);
 /// }
 /// # }
 /// ```
@@ -88,15 +91,35 @@ macro_rules! physical_device {
 }
 
 /// Creates a `Device` and a `Queue` for compute operations.
+///
+/// # Panics
+///
+/// Panics if no conpute-compatible queue has been found, or the
+/// device could not be initialized.
+///
+/// # Example
+///
+/// ```
+/// # extern crate vulkano;
+/// # #[macro_use]
+/// # extern crate vulkanology;
+///
+/// # #[allow(unused_variables)]
+/// # fn main() {
+/// let instance = instance!();
+/// let physical_device = physical_device!(instance);
+/// let (device, queue) = device_and_queue!(physical_device);
+/// # }
+/// ```
 #[macro_export]
 macro_rules! device_and_queue {
     ($physical_device:ident) => ({
         use vulkano::device::{Device, DeviceExtensions};
 
-        // Select a queue family which supports graphics.
+        // Select a queue family which supports compute operations.
         let mut queue_families = $physical_device.queue_families();
         let queue_family = queue_families.find(|q| q.supports_compute())
-            .expect("Couldn't find a graphical queue family.");
+                .expect("Couldn't find a compute queue family.");
 
         // Initialize a device and a queue.
         let device_extensions = DeviceExtensions::none();
@@ -104,9 +127,48 @@ macro_rules! device_and_queue {
                                                &$physical_device.supported_features(),
                                                &device_extensions,
                                                [(queue_family, 0.5)].iter().cloned())
-            .expect("Failed to create device.");
+                .expect("Failed to create device.");
 
+        // We only requested one queue, so `queues` is an array with only one element.
         (device, queues.next().unwrap())
+    })
+}
+
+/// Creates a new uninitialized buffer of type `$buf_type` of length `$buf_len`.
+///
+/// # Panics
+///
+/// If the array fails to be initialized.
+///
+/// # Examples
+///
+/// ```
+/// # extern crate vulkano;
+/// # #[macro_use]
+/// # extern crate vulkanology;
+///
+/// # #[allow(unused_variables)]
+/// # fn main() {
+/// let instance = instance!();
+/// let physical_device = physical_device!(instance);
+/// let (ref device, ref queue) = device_and_queue!(physical_device);
+///
+/// // Initialize a buffer.
+/// let buffer = cpu_array_buffer!(device, queue, u32, 13*31);
+/// # }
+/// ```
+#[macro_export]
+macro_rules! cpu_array_buffer {
+    ($device:ident, $queue:ident, $buf_type:ty, $buf_len:expr) => ({
+        use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
+        unsafe {
+            CpuAccessibleBuffer::<[$buf_type]>::uninitialized_array(
+                       $device,
+                       $buf_len,
+                       &BufferUsage::all(),
+                       Some($queue.family()))
+                   .expect("Failed to create a cpu accessible buffer.")
+        }
     })
 }
 
@@ -116,33 +178,71 @@ macro_rules! device_and_queue {
 /// it sets up a `ComputePipeline` and provides a function
 /// for executing the shader.
 ///
-/// # Examples
+/// # Panics
 ///
-/// 1. Invoke the `pipeline!` macro.
-///   * The macro parameters are:
-///     1. A three-dimensional array defining the workgroup count:
-///         `workgroup_count: [100, 100, 1],`
-///     2. The buffers that your test shader uses:
-///         `buffers: { a: [u32;4], b: [Dennis;42] },`
-///     3. The name of the shader execution:
-///         `execution_command: execute_shader_function_name_table_mouse`
-/// 2. Fill your buffers with input data.
-/// 3. Execute the shader.
-///     `execute_shader_function_name_table_mouse();`
+/// * If the instance, physical_device, device or queue cannot be selected/initialized.
+/// * If the buffers cannot be initialized. 
+/// * If the shader cannot be loaded.
+/// * If the pipeline cannot be created.
 ///
-/// 4. Assert validity of the results.
-///     `assert!(/*datainbuffersisvalid*/ true)`
+/// # Example
+///
+/// ```
+/// # extern crate vulkano;
+/// # #[macro_use]
+/// # extern crate vulkanology;
+///
+/// # #[allow(unused_variables)]
+/// # fn main() {
+/// // The total number of invocations of your shader is defined in two places:
+/// //      - The workgroup_count, which is defined in the pipeline macro.
+/// //      - The workgroup_size which is defined in the shader program header.
+///
+/// // Here we compute the total number of invocations. The workgroup size is 8x8x1,
+/// // and the workgroup count will be 100x100.
+/// let total_num_invocations = (8 * 8) * (100 * 100);
+///
+/// // I. Invoke the `pipeline!` macro.
+/// // The macro parameters are:
+/// //    1. The path to the shader program, relative to the crate root.
+/// //        `shader_path: "path/to/shader/program.comp"`
+/// //    2. A three-dimensional array defining the workgroup count:
+/// //        `workgroup_count: [1, 2, 3],`
+/// //    3. The buffers that your test shader uses:
+/// //        `buffers: { input_data: [u32;4], some_buffer: [Dennis;42] },`
+/// //    4. The name of the shader execution:
+/// //        `execution_command: run_example_shader_function_name`
+/// pipeline!{
+///     shader_path: "tests/shaders/example.comp",
+///     workgroup_count: [100, 100, 1],
+///     buffers: {
+///        data: [u32; total_num_invocations],
+///        result: [u32; total_num_invocations]
+///     },
+///     execution_command: execute_shader
+/// }
+///
+/// // 2. Fill your buffers with input data.
+/// {
+///     
+/// }
+/// // 3. Execute the shader.
+/// //    `run_example_shader_function_name();`
+///
+/// // 4. Assert validity of the results.
+/// //    `assert!(datainbuffersisvalid())`
+///
+/// # }
+/// ```
 ///
 #[macro_export]
 macro_rules! pipeline {
     {
+        shader_path: $shader_path:expr,
         workgroup_count: [$workgroup_x:expr, $workgroup_y:expr, $workgroup_z:expr],
-        buffers: {
-            $( $buf_ident:ident : [$buf_type:ty;$buf_len:expr] ),*
-        },
+        buffers: { $( $buf_ident:ident : [$buf_type:ty;$buf_len:expr] ),* },
         execution_command: $exec_cmd:ident
     } => {
-        use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
         use vulkano::command_buffer::PrimaryCommandBufferBuilder;
         use vulkano::command_buffer::submit as submit_command;
         use vulkano::descriptor::descriptor_set::DescriptorPool;
@@ -151,7 +251,7 @@ macro_rules! pipeline {
         // Include the shader wrapper.
         mod shader {
             #![allow(dead_code)]
-            include!{concat!(env!("OUT_DIR"), "/shaders/tests/shaders/random.comp")}
+            include!{concat!(env!("OUT_DIR"), concat!("/shaders/", $shader_path))}
         }
 
         // Create the pipeline layout wrapper.
@@ -169,19 +269,12 @@ macro_rules! pipeline {
         let (ref device, ref queue) = device_and_queue!(physical_device);
 
         // Allocate buffers.
-        $( let $buf_ident = unsafe {
-            CpuAccessibleBuffer::<[$buf_type]>::uninitialized_array(
-                       device,
-                       $buf_len,
-                       &BufferUsage::all(),
-                       Some(queue.family()))
-                   .expect("Failed to create cpu accessible buffer.")
-        }; )*
+        $( let $buf_ident = cpu_array_buffer!(device, queue, $buf_type, $buf_len); )*
 
         // Create descriptor pool.
         let descriptor_pool = DescriptorPool::new(device);
 
-        // Create pipeline.
+        // Create pipeline layout.
         let pipeline_layout = layout_definition::CustomPipeline::new(device).unwrap();
         let buffer_descriptors = layout_definition::buffers::Descriptors {
             $( $buf_ident: &$buf_ident, )*
@@ -190,20 +283,22 @@ macro_rules! pipeline {
                                                               &pipeline_layout,
                                                               &buffer_descriptors);
 
+        // Load the shader and assemble the pipeline.
         let compute_shader = shader::Shader::load(device).expect("Failed to create shader module.");
         let pipeline = ComputePipeline::new(device,
                                             &pipeline_layout,
                                             &compute_shader.main_entry_point(),
                                             &())
-            .expect("Failed to create compute pipeline.");
+                .expect("Failed to create compute pipeline.");
 
+        // Assemble and return the execution command.
         let workgroup_count = [$workgroup_x, $workgroup_y, $workgroup_z];
         let execution_command = PrimaryCommandBufferBuilder::new(device, queue.family())
             .dispatch(&pipeline, buffer_set, workgroup_count, &())
             .build();
-
         let $exec_cmd = || {
             submit_command(&execution_command, queue).unwrap();
         };
     }
 }
+
